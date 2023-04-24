bc166479a27a57cb079b8aa87f7f386989225129
phatttnm <124548061+phatttnm@users.noreply.github.com>
2023-04-24 11:26:39 +0700

    Add files via upload

---
+++ nonthaburi_travel.js
@@ -0,0 +1,155 @@
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เดอะมอลงามวงศ์วาน",
+          "type": "ศูนย์การค้า",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.5420237288348,
+            13.855179333528056
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เกาะเกร็ด",
+          "type": "วัฒนธรรม",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.47711865492892,
+            13.909187909757804
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ท่าน้ำนนท์",
+          "type": "วัฒนธรรม",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.49179871164012,
+            13.842307665001258
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "พิพิธภัณฑ์จังหวัดนนทบุรี",
+          "type": "ธรรมชาติ",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            100.4918308981479,
+            13.841515952811235
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ตลาดนกฮูก",
+          "type": "ศูนย์การค้า",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.4952335237075,
+            13.869409767957833
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เซนทรัลเวสเกต",
+          "type": "ศูนย์การค้า",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            100.41158238014395,
+            13.876987286702771
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "อีเกียบางใหญ่",
+          "type": "ศูนย์การค้า",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.41262779575442,
+            13.878690063465285
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เซนทรัลรัตนาธิเบศน์",
+          "type": "ศูนย์การค้า",
+          "level": 6
+        },
+        "geometry": {
+          "coordinates": [
+            100.49728850974941,
+            13.866360343627107
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เซนทรัลแจ้งวัฒนะ",
+          "type": "ศูนย์การค้า",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.52812725188204,
+            13.903598863685104
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "อุทยานเฉลิมกาญจนาภิเษก",
+          "type": "ธรรมชาติ",
+          "level": 6
+        },
+        "geometry": {
+          "coordinates": [
+            100.48110628764215,
+            13.849833358755433
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  } 
